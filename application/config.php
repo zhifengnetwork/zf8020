e1796d375e9cbccc8c9743ae94aa587454345492
Yang Zhongyi <80837879@qq.com>
2019-08-13 11:01:39 +0800

    修改后台登录

--- application/config.php
+++ application/config.php
@@ -141,14 +141,14 @@ return [
 
     // 视图输出字符串内容替换
     'view_replace_str'       => [
-        '__INSPINIA__' => '/static/inspinia',
-        '__IMG__'      => '/static/images',
-    	'__STATIC__'   => '/static',
-        '__LIB__'      => '/static/lib',
-        '__MOBILE__'   => '/static/mobile',
-        '__IMAGES__'   => '/upload/images',
+        '__INSPINIA__' => '/public/static/inspinia',
+        '__IMG__'      => '/public/static/images',
+    	'__STATIC__'   => '/public/static',
+        '__LIB__'      => '/public/static/lib',
+        '__MOBILE__'   => '/public/static/mobile',
+        '__IMAGES__'   => '/public/upload/images',
         '__PLIST__'    =>  '/plist',
-        '__PAGE__'    =>  '/static/page',
+        '__PAGE__'    =>  '/public/static/page',
 
     ],
     // 默认跳转页面对应的模板文件
@@ -197,7 +197,7 @@ return [
     //     // 缓存有效期 0表示永久缓存
     //     'expire'   => 0,
     // ],
-    
+
     // 文件缓存 (本地使用这个)
     'cache'                  => [
         // 驱动方式
@@ -223,7 +223,7 @@ return [
             //缓存前缀
             'prefix' => 'File_'
         ],
-        
+
         // redis缓存
         'redis'   =>  [
             // 驱动方式
@@ -286,7 +286,7 @@ return [
         0 => '待确认',
         1 => '已确认',
         2 => '已收货',
-        3 => '已取消',                
+        3 => '已取消',
         4 => '已完成',//评价完
         5 => '已作废',
         6 => '申请退款',
@@ -351,32 +351,32 @@ return [
         ],// 指定不能使用信用卡支付   不传入，则均可使用
 
         'fee_type'          => 'CNY',// 货币类型  当前仅支持该字段
-    
+
         'notify_url'        => 'http://api.puruitingxls.com/api/pay/weixin_notify/',
-    
+
         'redirect_url'      => 'http://www.puruitingxls.com/',// 如果是h5支付，可以设置该值，返回到指定页面
-    
+
         'return_raw'        => false,// 在处理回调时，是否直接返回原始数据，默认为true
 
         ],
 
 
 
-        
+
 
         'pay_config' => [
             'use_sandbox'               =>  false,// 是否使用沙盒模式
             'app_id'                    => '2019050264367537',
             'sign_type'                 => 'RSA2',// RSA  RSA2
-        
+
             // ！！！注意：如果是文件方式，文件中只保留字符串，不要留下 -----BEGIN PUBLIC KEY----- 这种标记
             // 可以填写文件路径，或者密钥字符串  当前字符串是 rsa2 的支付宝公钥(开放平台获取)
             'ali_public_key'            => 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArLvewr55lEYE7iL7vyZkDjZZnofIwRtMQJNBpBOJX8HSum9igthE0wMyz+YmYV7xq1utIrRLy5Zmt991CYyaOCRnzwqrv8ZlEG38QhSwtXXlnxJiqFF5u81K6DXpFAvHYdyPmcAW/sVWE+5X4S4HFvfCvndN6RGih+YdDjzcVi1uIV1JKQG6q6l8xHRiW7EGtGvJW5Q0DAwZ6VAogKB8PIyvuRrRaO3uPezbYcyAZMOJhqkyR/18ji1W0e0Qf3GNKuF+tHJjOTPJ318LvAcuMwLm6hoscD8G6PYwxAgoZ9auYwtjLBbz8a0aQcIX51WInOXrQOBzIZuLdzggqC+2AQIDAQAB',
-        
+
             // ！！！注意：如果是文件方式，文件中只保留字符串，不要留下 -----BEGIN RSA PRIVATE KEY----- 这种标记
             // 可以填写文件路径，或者密钥字符串  我的沙箱模式，rsa与rsa2的私钥相同，为了方便测试
             'rsa_private_key'           => 'MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCNrD7Mh6r0hTTubDrb6rXFUcbGtSnIY9ZyGnqglAigrH6h2fyYC+LzJm8D+J2P1yNUn1TO/7SKxo5ImgjunX0RH3BsO+a+1cOj5tZ88Y1GdlPLHOt9EP09U2/EDJXD6ucJ9sb1WFXeZiWXR52UJFcklgk2F//2NCdMHGYyllFrRJ0fYjF1wOCR/t4UlTNNm51H2hbXO8VxmNX4ZVDy8B6dhM0fNay/bUj2jUSu8/Q45vZQbH4OkRb4wSNtgVcejKpqhSdsajYekKTH0uWIB6FKCus8pKwe6SlCP0DhVF8RLmw1AzmcPbKAXiE4VazEbwTsRc3MJ6vFNEeOAKxF9njdAgMBAAECggEAVhak0oReTdfkIj2CRsCJVC4tK/JKQYrpdMzCV3GdDIXFLXTZGUufzUE9lJwuoomI3pMzZdXcT7f4HgX8B4OLzCvelOaRgMVE7QQIskPWJUsh//rC3mzEdc+NywQavcKwQk3C+LOE+m/3x8Ws66hpi8HgNw6+a02l04ouT+8n6pYM1f+4Vy7Fb4LdDEkzKDkvtxj+czXDp/1gnRExvqpMkK9iwFfmvO4rO8Ubw+URjIX6vuIZytbnZqT1jpJNfmvigWPplOdRiyVk5YVtBKNAQYSlk8y6Tiftq9VuJEmRuSs3ohoxS9h128Fbze2/oYDtwLGZKXh2D6GivE2Xz9CGAQKBgQDezHVMJdNjIdO0iUh8oW0li9NOYkl1Bsk4ICKPiKWTEx5OT/qbvO/VZskUaHauWgwhMN12wkM6UI0tBonO/ZOON7Fh/5bQCC5QKHtN//EHz932sKyO/qHgCiWzib30jSZ5XtBx8ClPiKfDIu+UaZXlgCLgoi0fxcfyvWQXL7mDmQKBgQCiyOtdF1Alt0oKSE1v5qARmZ4ldb4FWWwaVQJgocEP11wwIQhq3y4ZgHBrDJP35ec7Hyrlr1i/+hjVd3nEtEzt1F4Jj0aNXHwlch+fsfKR1eN5EcP6zmS6idj/239w+lTGN0eIylvBW/J811E7VF1lWgnesYcmOXxCG+XJdJpp5QKBgDj1xqtAJGn8tPY7/tc2IgRuWgh5IlST9o+tz4gopEQUqDPXSLfWNu61B4V7K5Rpmx5FMulwwuU+wMkZGdRcigPbAzONt43Z+ZUutE99tq6LmzC9fHBWcyYnEfpzpafHCmYPMnVetAEMa+98mAm2cMcq2j/Z1nWACB1sBBHVdrVJAoGBAIkNUiPRQfhPJfYcQ54n9LJ8vIpbZD3KuNo+oj7LUOlOb15SIW0hNAXifkOSlm3LUXAUYKB6jeUr4oavDYVQK8i82OOBjmvr5tX8DKX+QvUHuHmxPGhIJsRq1Jktq1FqYb90wTRo8vGLwU/cVJb4A54WPWMR4nCLS5O5OzDujCcFAoGAARLKIuYqRdyemWv2LaHCvVd+r9T8uyzRJK/udX8hb6mfScCyQeLsMj6oavlWioTGVDZb1zHjd8Uo64buGyXsBqzmgQXPtt3w3Vgs+WvfpUCzSCsPalhSsCaPKRvxYtoxZ/HepwegX77aT3sT6sERIEksl9wyClV5Q3mqP5JYI8k=',
-        
+
             'limit_pay'                 => [
                 //'balance',// 余额
                 //'moneyFund',// 余额宝
